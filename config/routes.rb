508764a9170371a9da7f8592e43e26f9720f82b1
sraskin <shuvho.91@gmail.com>
2015-12-13 12:58:05 +0600

    added css on deevice services and put condition, without sign in no options will be available

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :courses
   root 'home#index'
-  resources :home
-
+  get 'home/index'
+  get 'home/offer_status'
+  get 'home/mail'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
